c310a524c956c86f0ce537379e3cc17dd0bb3563
Jens Dieskau <jfreax@googlemail.com>
2013-02-11 12:25:43 +0100

    Basic documentation support and license text

--- src/devices/eeprom/spi_eeprom.h
+++ src/devices/eeprom/spi_eeprom.h
@@ -1,5 +1,38 @@
+/*
+    Copyright (C) 2013  Jens Dieskau <jens.dieskau@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef SPI_EEPROM_H
 #define SPI_EEPROM_H
+
+/** 
+ *  @file
+ *  @code #include <spi_eeprom.h> @endcode
+ *  @ingroup eeprom
+ * 
+ *  @brief This is
+ *         and more
+ *         here
+ *
+ *  @note Based on 
+ *  @author Jens Dieskau jens.dieskau@gmail.com http://jdsoft.de
+ */
+ 
+/**@{*/
 
 #include "defines.h"
 
@@ -7,10 +40,17 @@
 #define OP_WRITE  1
 #define OP_EREASE 3
 
+
+/**
+ * lala
+ */
 int16_t EEPROM_read(uint8_t adress);
 void EEPROM_write(uint8_t adress, int16_t data);
 
 void EEPROM_write_enable(void);
 
 
-#endif
+
+/**@}*/
+
+#endif // SPI_EEPROM_H
--- src/main.h
+++ src/main.h
@@ -16,39 +16,10 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef SPI_H
-#define SPI_H
+/*! @defgroup spi SPI
+    @brief Documentation for SPI. 
+*/
 
-#include "defines.h"
-
-#if defined(MCU_328P)
-#define SPI_PORT PORTB
-#define SPI_DDR  DDRB
-#define SPI_MOSI DDB3
-#define SPI_MISO DDB4
-#define SPI_SCK  DDB5
-#define SPI_CS   DDB2
-#endif
-
-
-/**
- * Initialize register for master spi communication
- */
-void SPI_MasterInit(void);
-
-
-/**
- * Shift full array to target device without receiving any byte
- * @param 8-Bit data
- */
-uint8_t SPI_MasterTransmit(uint8_t cData);
-
-
-/**
- * Read one full byte from slave
- * @return MISO byte
- */
-uint8_t SPI_MasterReceive(void);
-
-
-#endif
+/*! @defgroup eeprom EEPROM
+    @brief Documentation for EEPROM. 
+*/
