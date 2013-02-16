95ef286a755cae58eabeba5d4b20d16921c196a1
Jens Dieskau <jfreax@googlemail.com>
2013-02-16 16:48:22 +0100

    Modular font include

--- src/data/font.h
+++ src/data/font.h
@@ -16,60 +16,10 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef DEFINES_H
-#define DEFINES_H
+#include "defines.h"
+#if !defined(DATA_FONT_H) && defined(JAL_INCLUDE_FONT)
+#define DATA_FONT_H
 
-/**
- * General
- */
-#ifndef F_CPU
-#define F_CPU 16000000L
-#endif // F_CPU
+extern unsigned char simpleFont[][8];
 
-/** 
- * Enable features.
- * Comment out unused features
- */
-#define JAL_USE_UART
-#define JAL_USE_TWI
-#define JAL_USE_SPI
-#define JAL_USE_PRINTF
-
-#define JAL_INCLUDE_FONT
-
-/** 
- * Enable hardware device driver.
- * Comment out unused drivers.
- */
-#define JAL_USE_EEPROM_93C66
-#define JAL_USE_SSD1306
-
-
-
-/**
- * UART specific
- */
-#ifndef UART_BAUD_RATE
-#define UART_BAUD_RATE 57600
-#endif // UART_BAUD_RATE
-
-
-/**
- * KDevelop extras
- */
-#ifdef IN_IDE_PARSER
-
-#define MCU_328P
-
-#if defined(MCU_328P) && !defined (_AVR_IOM328P_H_)
-#include <avr/iom328p.h>
-#endif
-
-#if defined (MCU_1280) && !defined (_AVR_IOM1280_H_)
-#include <avr/iom1280.h>
-#endif
-
-#endif
-
-
-#endif // DEFINES_H
+#endif // DATA_FONT_H
